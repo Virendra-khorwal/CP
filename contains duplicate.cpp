97fccfa2f2ea1b7d1c26271cb3f83a800acf21c8
Sarthak Bhardwaj <7sarthakbhardwaj@gmail.com>
2020-10-19 13:23:26 +0530

    Update contains duplicate.cpp

--- contains duplicate.cpp
+++ contains duplicate.cpp
@@ -8,3 +8,13 @@ if(n[i]==n[i+1])return true;
 return false;}
 
 };
+
+
+/*class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+if(nums.size()==0)return false;
+set<int> s(nums.begin(), nums.end());
+        if(s.size() < nums.size())return true;
+        return false;}
+}; */
