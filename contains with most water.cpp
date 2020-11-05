dfd4ad71534081a3c4abee5f63bd20f657a2526e
Sarthak Bhardwaj <7sarthakbhardwaj@gmail.com>
2020-11-05 23:55:52 +0530

    Add files via upload

---
+++ contains with most water.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int l = 0;
+        int r = height.size()-1 ;
+        
+        int area = 0;
+        while(l<r){
+            if(height[l]>height[r]){
+             area = max(area,height[r]*(r-l));
+                r--;
+            }
+            else{
+                area = max(area,height[l]*(r-l));
+                l++;
+            }
+        }
+   return area;
+    }
+};
