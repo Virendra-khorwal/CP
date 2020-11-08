9c54ec25f6e59810a631477e303b944ef444d888
Sarthak Bhardwaj <7sarthakbhardwaj@gmail.com>
2020-11-08 16:05:35 +0530

    Add files via upload

---
+++ counting 1 bits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans=0;
+        while(n>0){
+            n = n & (n-1);
+            ans++;
+        }
+        return ans;
+    }
+};
+
+//second
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        return __builtin_popcount(n);
+    }
+};
