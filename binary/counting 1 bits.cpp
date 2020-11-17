1fffe25c5c201cc31ae6814a6839fca1ae7fcecf
Sarthak Bhardwaj <7sarthakbhardwaj@gmail.com>
2020-11-17 23:01:58 +0530

    Update counting 1 bits.cpp

--- binary/counting 1 bits.cpp
+++ binary/counting 1 bits.cpp
@@ -17,3 +17,27 @@ public:
         return __builtin_popcount(n);
     }
 };
+
+//implementation
+#include<bits/stdc++.h>
+
+using namespace std;
+
+bool check(int n){
+    return(n && !n & n-1);
+}
+
+int one_count(int k){
+    int count = 0;
+   while(k){
+    k = k & k-1;
+    count++;
+   }
+    return count;
+}
+
+
+int main(){
+
+cout << one_count(3);
+}
