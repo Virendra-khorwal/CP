2fa9bdb8fa69389c40eb2968dd9bedfb6b1eb8b8
1sarthakbhardwaj <7sarthakbhardwaj@gmail.com>
2021-04-20 12:32:35 +0530

    addition

---
+++ HR/point.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    pair<int,int>point;
+
+    cout << "anything : ";
+    cin >> point.first;
+    cin >> point.second;
+
+    cout << point.first<<point.second;
+
+}
