56d2501aaa0d01eb1eaabc0c15cff2de110d8f17
Sarthak Bhardwaj <7sarthakbhardwaj@gmail.com>
2021-09-18 16:30:00 +0530

    Create Valid_paranthesis.cpp

---
+++ string/Valid_paranthesis.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool isValid(string s) {
+     stack<char> x;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]=='('||s[i]=='{'||s[i]=='[')
+             x.push(s[i]);
+            else if(!x.empty())
+            {
+            if(s[i]==']'&&x.top()=='[')
+            {
+                x.pop();
+            } 
+             else if(s[i]=='}'&&x.top()=='{')
+            {
+                x.pop();
+            } 
+             else if(s[i]==')'&&x.top()=='(')
+            {
+                x.pop();
+            } 
+            else
+            x.push(s[i]);
+            } 
+            else
+            return false;
+        } 
+       return x.empty(); 
+        
+    }
+};
