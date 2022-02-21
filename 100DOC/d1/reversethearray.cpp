c68e7c0537fec70749c3c8d4269910421271b459
Sarthak Bhardwaj <7sarthakbhardwaj@gmail.com>
2022-02-21 14:20:04 +0530

    Create reversethearray.cpp

---
+++ 100DOC/d1/reversethearray.cpp
@@ -0,0 +1,9 @@
+void reverseArray(vector<int> &arr , int m)
+{
+    int s = arr.size();
+    for(int i=m+1;i<s;i++){
+        swap(arr[i], arr[n-1]);
+        s--;
+    }
+	// Write your code here.
+}
